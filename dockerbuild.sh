728a909d4737dc61d967d4a4b044e36f0d9f692b
Hritik <hritikm46@gmail.com>
2021-02-25 11:28:02 +0530

    build 0.2.1-RC3

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,5 +1,5 @@
 mvn clean install -DskipTests
 
-docker build -t lexplatform.azurecr.io/user-automation:0.2.1-RC2 .
+docker build -t lexplatform.azurecr.io/user-automation:0.2.1-RC3 .
 
-docker push lexplatform.azurecr.io/user-automation:0.2.1-RC2
+docker push lexplatform.azurecr.io/user-automation:0.2.1-RC3
