2ad00400fc5ecda2cb7b4aed781de0fb88a5c168
Hritik <hritikm46@gmail.com>
2021-03-17 16:06:15 +0530

    0.2.1-RELEASE version

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,5 +1,5 @@
 mvn clean install -DskipTests
 
-docker build -t lexplatform.azurecr.io/user-automation:0.2.1-RC3 .
+docker build -t lexplatform.azurecr.io/user-automation:0.2.1-RELEASE .
 
-docker push lexplatform.azurecr.io/user-automation:0.2.1-RC3
+docker push lexplatform.azurecr.io/user-automation:0.2.1-RELEASE
