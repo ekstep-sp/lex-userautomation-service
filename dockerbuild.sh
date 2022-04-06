505375ff56019ca9ff060a7bdb95afe502b545fb
Thyagu <rajan.tech.86@gmail.com>
2022-04-06 20:48:42 +0530

    Final commit
    Final commit

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,5 +1,5 @@
 mvn clean install -DskipTests
 
-docker build -t lexplatform.azurecr.io/user-automation:space-v3-sprint-1 .
+docker build -t lexplatform.azurecr.io/user-automation:space-v3-sprint-4.2 .
 
-docker push lexplatform.azurecr.io/user-automation:space-v3-sprint-1
+docker push lexplatform.azurecr.io/user-automation:space-v3-sprint-4.2
